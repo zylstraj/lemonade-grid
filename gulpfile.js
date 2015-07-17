bb64540f2ae3fea7d7616a314ecd8a2880ec777e
Josh Zylstra <zylstrajosh@gmail.com>
2015-07-17 03:38:13 -0700

    starting to play with Sass and more redesigning of index page

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./public/sass/application.scss')
+  gulp.src('./public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/application.css'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./public/sass/application.scss', ['sass']);
+  gulp.watch('./public/css', ['sass']);
 });
